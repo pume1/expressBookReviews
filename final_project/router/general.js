97af98ba80107cfda118768add19270563a4b216
pume1 <67070039@kmitl.ac.th>
2026-07-31 00:11:44 -0400

    mwa

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,50 +24,52 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', async function (req, res) {
-  try {
-      // จำลองการดึงข้อมูลแบบ Async ด้วย Promise
-      const getBooks = await new Promise((resolve, reject) => {
-          resolve(books);
-      });
-      return res.status(200).send(JSON.stringify(getBooks, null, 4));
-  } catch (error) {
-      return res.status(500).json({message: "Error fetching books"});
-  }
+public_users.get('/', function (req, res) {
+    let myPromise = new Promise((resolve, reject) => {
+        resolve(books);
+    });
+    myPromise.then((bks) => {
+        return res.status(200).send(JSON.stringify(bks, null, 4));
+    });
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', async function (req, res) {
-  const isbn = req.params.isbn;
-  try {
-      const getBookByISBN = await new Promise((resolve, reject) => {
-          if (books[isbn]) {
-              resolve(books[isbn]);
-          } else {
-              reject("Book not found");
-          }
-      });
-      return res.status(200).json(getBookByISBN);
-  } catch (error) {
-      return res.status(404).json({message: error});
-  }
+public_users.get('/isbn/:isbn', function (req, res) {
+    let myPromise = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        if (books[isbn]) {
+            resolve(books[isbn]);
+        } else {
+            reject("Book not found");
+        }
+    });
+    myPromise.then((book) => {
+        return res.status(200).json(book);
+    }).catch((err) => {
+        return res.status(404).json({ message: err });
+    });
 });
   
 // Get book details based on author
-public_users.get('/author/:author', async function (req, res) {
-  const author = req.params.author;
-  try {
-      await axios.get('http://localhost:5000/').catch(err => {}); 
-      let filteredBooks = [];
-      for (let isbn in books) {
-          if (books[isbn].author === author) {
-              filteredBooks.push({ "isbn": isbn, "author": books[isbn].author, "title": books[isbn].title, "reviews": books[isbn].reviews });
-          }
-      }
-      return res.status(200).json(filteredBooks);
-  } catch (error) {
-      return res.status(500).json({message: error});
-  }
+public_users.get('/author/:author', function (req, res) {
+    let myPromise = new Promise((resolve, reject) => {
+        const author = req.params.author;
+        let filteredBooks = [];
+        for (let isbn in books) {
+            if (books[isbn].author === author) {
+                filteredBooks.push({
+                    "isbn": isbn,
+                    "author": books[isbn].author,
+                    "title": books[isbn].title,
+                    "reviews": books[isbn].reviews
+                });
+            }
+        }
+        resolve(filteredBooks);
+    });
+    myPromise.then((filtered) => {
+        return res.status(200).json(filtered);
+    });
 });
 
 // Get all books based on title
